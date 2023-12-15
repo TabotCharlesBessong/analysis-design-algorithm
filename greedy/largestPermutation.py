9dfa4ad2c3621280077df3090212775007738a14
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-12-15 07:45:16 +0100

    fixing an indentation problem

--- greedy/largestPermutation.py
+++ greedy/largestPermutation.py
@@ -6,16 +6,16 @@ def largestPermutation(self,A,B):
   
   while B and i < len(A):
     j = d[_max]
-      if i == j:
-        pass
-      else:
-        B -= 1
-        A[i] , A[j] = A[j], A[i]
-        d[A[i]] , d[A[j]] = d[A[i]]
-        
-      i += 1
-      _max -= 1  
+    if i == j:
+      pass
+    else:
+      B -= 1
+      A[i] , A[j] = A[j], A[i]
+      d[A[i]] , d[A[j]] = d[A[i]]
+      
+    i += 1
+    _max -= 1  
       
     return A
   
-# largestPermutation([2,3,5,1], 4)
+largestPermutation([2,3,5,1], 4)
